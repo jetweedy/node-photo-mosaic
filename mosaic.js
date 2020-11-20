dd2275d268912e29bd1d48df7c1123b1f9cfee5b
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-11-20 11:20:48 -0500

    Add files via upload

--- mosaic.js
+++ mosaic.js
@@ -1,6 +1,13 @@
 //// https://www.npmjs.com/package/mosaic-node-generator
 var mosaic = require('mosaic-node-generator');
-mosaic.mosaic( 
-  'beckett.jpg', 
-  'images' 
-);
+var args = process.argv;
+if (args.length > 2) {
+    mosaic.mosaic( 
+      args[2],
+      'images',
+      10, 
+      10,
+    );
+} else {
+    console.log("Please provide source image.");
+}
