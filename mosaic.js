ee2118dd4d060d109e978c2559a99f88708b1c2c
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-11-20 15:01:08 -0500

    Add files via upload

--- mosaic.js
+++ mosaic.js
@@ -2,12 +2,22 @@
 var mosaic = require('mosaic-node-generator');
 var args = process.argv;
 if (args.length > 4) {
+
+    var s = parseInt(args[4]);
+    var sizeOf = require('image-size');
+    var dimensions = sizeOf(args[2]);
+    var c = Math.ceil(Math.max(dimensions.width, dimensions.height) / s) * Math.ceil(s/10);
+    var r = c;
+
     mosaic.mosaic( 
       args[2],
       args[3],
-      parseInt(args[4]),
-      parseInt(args[4]),
+      s,
+      s,
+      c,
+      r,
     );
+
 } else {
     console.log("Please provide source image.");
 }
