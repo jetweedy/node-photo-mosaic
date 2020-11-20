5aad51c1cddadabc9e4e125ae1143091af16f31d
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-11-20 15:18:42 -0500

    Add files via upload

--- mosaic.js
+++ mosaic.js
@@ -2,18 +2,14 @@
 var mosaic = require('mosaic-node-generator');
 var args = process.argv;
 if (args.length > 4) {
-
-    var s = parseInt(args[4]);
-    var sizeOf = require('image-size');
-    var dimensions = sizeOf(args[2]);
-
+//    var s = parseInt(args[4]);
+//    var sizeOf = require('image-size');
+//    var dimensions = sizeOf(args[2]);
     mosaic.mosaic( 
       args[2],
       args[3],
-      s,
-      s,
+      parseInt(args[4]),
     );
-
 } else {
     console.log("Please provide an image file, a folder full of images to use as tiles, and a numeric tile size value.");
 }
