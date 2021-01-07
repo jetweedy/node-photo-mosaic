fea3787d37bb7d4af43f7dfc9d06ba35799360c3
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2021-01-07 13:14:35 -0500

    Add files via upload

--- mosaic.js
+++ mosaic.js
@@ -1,3 +1,6 @@
+/*
+# node mosaic.js <image> <thumbsFolder> <tileHeightAndWidth> <numColsAndRows>
+*/
 //// https://www.npmjs.com/package/mosaic-node-generator
 var mosaic = require('mosaic-node-generator');
 var args = process.argv;
@@ -9,6 +12,9 @@ if (args.length > 4) {
       args[2],
       args[3],
       parseInt(args[4]),
+      parseInt(args[4]),
+      parseInt(args[5]),
+      parseInt(args[5]),
     );
 } else {
     console.log("Please provide an image file, a folder full of images to use as tiles, and a numeric tile size value.");
